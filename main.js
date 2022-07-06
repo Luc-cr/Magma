0cdb9758d9448564e0e099e05c593568fdab116b
Luc-cr <72467699+Luc-cr@users.noreply.github.com>
2022-07-06 18:07:53 -0300

    LoginCommit

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hola");
+const express = require("express");
+let app = express();
+const port = 4000;
+
+app.get("/login", (req,res)=>{
+    res.sendFile(__dirname + "/FrontEnd/login/index.html");
+});
+
+app.listen(port, ()=>{
+    console.log("Escuchando en el puerto 4000 a conexiones entrantes");
+});
