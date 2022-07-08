87923caef81f020509e4f524d28410c223cfebef
Lucas <72467699+Luc-cr@users.noreply.github.com>
2022-07-08 12:03:47 -0300

    LoginStart

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const { application } = require('express');
 const express = require('express');
 const app = express();
+const port = 3000;
 
 app.get("/login", (req,res)=>{
     res.json({"status":"ok","data":[1,2,3]});
 });
+app.post("/login", (req,res)=>{
+    console.log(req.header);
+    res.json({"status": "ok"});
+});
 
-
-app.listen(5000, ()=>{console.log("Se inicio el servidor en el puerto 5000")});
+app.listen(port, ()=>{console.log("Se inicio el servidor en el puerto:", port)});
